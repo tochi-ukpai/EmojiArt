a3438d4d074eef5f644c78f43a545f1ef9fa43d5
Tochukwu Ukpai <tochukwuukpai@gmail.com>
2023-05-15 09:37:10 +0100

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Theós on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
